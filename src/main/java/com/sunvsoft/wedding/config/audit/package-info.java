1bb1100ba1253556ff284439d5ca3015d22cfe47
renwenqiang <renwenqiang@f6848f2b-db82-49f4-a46f-57bc13dda0ea>
2017-11-24 07:28:16 +0000

    新版婚祝后台

---
+++ src/main/java/com/sunvsoft/wedding/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.sunvsoft.wedding.config.audit;
---
+++ src/main/java/com/sunvsoft/wedding/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.sunvsoft.wedding.web.rest.vm;
